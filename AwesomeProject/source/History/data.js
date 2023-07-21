b897b75cb09a794cce559e2b3221c69b5e6a3659
Long Huynh <longbhuynh019@gmail.com>
2023-07-21 04:12:50 -0500

    updated list of items

---
+++ AwesomeProject/source/History/data.js
@@ -0,0 +1,8 @@
+export default items = [
+  { id: 0, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+  { id: 1, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+  { id: 2, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+  { id: 3, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+  { id: 4, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+  { id: 5, photo: "", sellers: ["Amazon", "eBay", "Facebook", "Walmart"] },
+];
---
+++ AwesomeProject/source/History/resultPage.js
@@ -0,0 +1,97 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableWithoutFeedback,
+  FlatList,
+} from "react-native";
+import items from "./data.js";
+import { Avatar, Button, Card } from "react-native-paper";
+
+
+
+
+
+
+
+export default function ResultPage() {
+  const [clickViewSellers, setClickViewSellers] = useState(false);
+
+
+  const renderItem = ({ item }) => (
+  
+  <Card style={styles.item_card}>
+     
+      <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+      <Card.Actions>
+      <Button>Sellers</Button>
+   
+    </Card.Actions>
+  </Card>
+);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>History</Text>
+      <View style = {{height: 660, top: 100}}>
+      <FlatList
+        
+        data = {items}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+
+
+      </View>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    lineHeight: 40,
+    fontWeight: "bold",
+    letterSpacing: 0.2,
+    color: "white",
+    backgroundColor: "black",
+    padding: 12,
+    borderRadius: 20,
+    overflow: "hidden",
+    top: 200,
+  },
+  title: {
+    fontSize: 50,
+    lineHeight: 40,
+    fontWeight: "bold",
+    letterSpacing: 0.2,
+    color: "black",
+    position: "fixed",
+    padding: 12,
+    borderRadius: 20,
+    overflow: "hidden",
+    top: 100,
+    right: 90,
+    backgroundColor: "white"
+  },
+  item_card: {
+    top: 120,
+    width: 320,
+    marginBottom: 30,
+  
+  },
+  list: {
+    height: 100,
+    overflow: "hidden"
+  }
+});
