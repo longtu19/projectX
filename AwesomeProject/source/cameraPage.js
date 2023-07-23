aca1aefc802d3fd384319875841ce6702e1fc7a0
Giang Nguyen <gn02.nguyen@gmail.com>
2023-07-23 13:32:19 +0700

    Using react native vision instead of react native camera.

--- AwesomeProject/source/cameraPage.js
+++ AwesomeProject/source/cameraPage.js
@@ -1,60 +1,14 @@
-import React, {PureComponent} from 'react';import {RNCamera} from 'react-native-camera';
-import Icon from 'react-native-vector-icons/dist/FontAwesome';import {TouchableOpacity, Alert, StyleSheet} from 'react-native';
-export default class Camera extends PureComponent {
-  constructor(props) {
-    super(props);
-      this.state = {takingPic: false,};
-  }
+import React from 'react';
+export function Camera() {
+  const devices = useCameraDevices('wide-angle-camera')
+  const device = devices.back
 
-  takePicture = async () => {
-    if (this.camera && !this.state.takingPic) {
-
-      let options = {
-        quality: 0.85,
-        fixOrientation: true,
-        forceUpOrientation: true,
-      };
-
-      this.setState({takingPic: true});
-
-      try {
-         const data = await this.camera.takePictureAsync(options);
-         Alert.alert('Success', JSON.stringify(data));
-      } catch (err) {
-        Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
-        return;
-      } finally {
-        this.setState({takingPic: false});
-      }
-    }
-  };
-  render() {
-    return (
-      <RNCamera 
-        ref = {ref => {this.camera = ref}}
-        captureAudio={false}
-        style={{flex: 1}}
-        type={RNCamera.Constants.Type.back}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }} ></RNCamera>
-    )
-    //     activeOpacity=0.5
-    //     style= styles.btnAlignment
-    //     onPress=this.takePicture>
-    //     <icon name="camera" size="{50}" color="#fff"></icon>
-    // );
-  }
+  if (device == null) return <LoadingView />
+  return (
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
+  )
 }
-const styles = StyleSheet.create({
-   btnAlignment: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-      marginBottom: 20,
-    },
-});
